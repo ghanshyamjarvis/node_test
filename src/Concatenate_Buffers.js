7d678afc87c75736ea8f9e2521787eacdbbc9ef6
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2019-12-11 19:01:57 +0530

    Concatenate_Buffer

---
+++ src/Concatenate_Buffers.js
@@ -0,0 +1,13 @@
+
+/*
+Syntax
+Buffer.concat(list[, totalLength])
+*/
+
+var buffer1 = new Buffer('TutorialsPoint ');
+
+var buffer2 = new Buffer('Simply Easy Learning');
+
+var buffer3 = Buffer.concat([buffer1,buffer2]);
+
+console.log("buffer3 content: " + buffer3.toString());
