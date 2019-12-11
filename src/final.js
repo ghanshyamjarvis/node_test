405459ab4cf9cfb0618f67addc9ceb431f736a76
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2019-12-11 17:59:07 +0530

    Module Call File

---
+++ src/final.js
@@ -0,0 +1,10 @@
+const final = require ('./stuff')
+
+console.log(final.counter(["raj","shyam","Meet","Hiren"]))
+console.log(final.sum(5,6))
+console.log(final.pi)
+
+
+console.log(final.three([10,25,6,47,9,9,74,3]))
+console.log(final.four(5,6))
+console.log(final.pi)
